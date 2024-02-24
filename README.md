760bee4ce39a897c1e448c622f6bd27427642001
Pummelhead <pummelhead@gmail.com>
2024-02-23 23:39:13 -0700

    fixed what weather API call I was making to be forecast for now and 6 hours from now

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
 # commute_info
 Weather and traffic info about commute
 
-I use a file called private_info.py to store the values of my home and work zip code as well as my API keys.  
+I'm pulling info from OpenWeather and Google Maps
+
+I use a file called private_info.py to store the values  
+of my home and work zip code as well as my API keys.  
 If you look at the variable names I import that will give you an idea  
 of how to set up your variables.  
   
 You will need the requests library. pip install requests
+
+With how the OpenWeather API works I plan on having the  
+program run in the morning to give me the "current" temp  
+and the "high" which will be 6 hours from the "current"
--- weather_functions.py
+++ weather_functions.py
@@ -2,32 +2,33 @@ import requests
 from private_info import home_zip, work_zip, weather_api_key
 
 def make_weather_home_call():
-    api_endpoint = f"https://api.openweathermap.org/data/2.5/weather?zip={home_zip}&appid={weather_api_key}&units=imperial"
+    api_endpoint = f"https://api.openweathermap.org/data/2.5/forecast?zip={home_zip}&appid={weather_api_key}&units=imperial"
     home_weather_response = requests.get(api_endpoint)
     if home_weather_response.status_code == 200:
         print("Home Weather Pull Completed")
         home_weather_data = home_weather_response.json()
-        current_temp_home = home_weather_data["main"]["temp"]
-        max_temp_home = home_weather_data["main"]["temp_max"]
-        home_location = home_weather_data["name"]
+        current_temp_home = home_weather_data["list"][0]["main"]["temp"]
+        max_temp_home = home_weather_data["list"][2]["main"]["temp"]
+        home_location = home_weather_data["city"]["name"]
         return home_location, current_temp_home, max_temp_home
     else:
         print(f"Error: {home_weather_response.status_code}")
         return None  
 
 def make_weather_work_call():
-    api_endpoint = f"https://api.openweathermap.org/data/2.5/weather?zip={work_zip}&appid={weather_api_key}&units=imperial"
+    api_endpoint = f"https://api.openweathermap.org/data/2.5/forecast?zip={work_zip}&appid={weather_api_key}&units=imperial"
     work_weather_response = requests.get(api_endpoint)
     if work_weather_response.status_code == 200:
         print("Work Weather Pull Completed")
         work_weather_data = work_weather_response.json()
-        current_temp_work = work_weather_data["main"]["temp"]
-        max_temp_work = work_weather_data["main"]["temp_max"]
-        work_location = work_weather_data["name"]
+        current_temp_work = work_weather_data["list"][0]["main"]["temp"]
+        max_temp_work = work_weather_data["list"][2]["main"]["temp"]
+        work_location = work_weather_data["city"]["name"]
         return work_location, current_temp_work, max_temp_work
     else:
         print(f"Error: {work_weather_response.status_code}")
         return None
 
 if __name__ == "__main__":
+    print(make_weather_home_call())
     print(make_weather_work_call())
