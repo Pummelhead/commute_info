5ce3ffa24ae080c9b4dc030a0111ebeb9831a5c9
Pummelhead <pummelhead@gmail.com>
2024-03-12 22:47:26 -0600

    fixed trip duration by updating api endpoint to include now as departure time rather than it pulling a default value.

--- traffic_functions.py
+++ traffic_functions.py
@@ -2,7 +2,7 @@ import requests
 from private_info import maps_api_key, home_address, work_address
 
 def make_traffic_call():
-    api_endpoint = f"https://maps.googleapis.com/maps/api/directions/json?destination={work_address}&origin={home_address}&key={maps_api_key}"
+    api_endpoint = f"https://maps.googleapis.com/maps/api/directions/json?departure_time=now&destination={work_address}&origin={home_address}&key={maps_api_key}"
     traffic_response = requests.get(api_endpoint)
     if traffic_response.status_code == 200:
         print("Traffic Pull Completed")
